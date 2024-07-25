864dc815e9b9d17312afb2d76bda37678140f65e
josepedro10 <145406929+josepedro10@users.noreply.github.com>
2024-07-25 15:43:13 -0300

    Add files via upload

---
+++ maricota/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maricota</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="./imagens/nova.jpg" alt="">
+        </div>
+    </header>
+    
+    <main>
+        <div class="formu">
+        <h3>Acesse sua conta</h3>
+        <form action="listacliente.php" method="post">
+        
+        <div class="email">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Email" required>
+        </div>
+        
+       <div class="senha">
+       <label for="password">Senha</label>
+       <input type="password" id="password" name="password" placeholder="Senha" required>
+       </div>
+        
+       <div class="entrar">
+       <a href="produto.php" class="button-link"> Entrar</a>
+       </div>
+        </form>
+        <a href="cadcliente.php" class="button-link">Cadastrar conta</a>
+        </div>
+        
+
+    </main>
+    
+    <footer>
+    <div class="footer-container">
+            <div class="footer-column">
+                <h3>Sobre Nós</h3>
+                <p>Aqui na Loja de Sapatos, oferecemos a melhor seleção de calçados para todos os estilos e ocasiões. Qualidade e conforto garantidos.</p>
+            </div>
+            <div class="footer-column">
+                <h3>Links Úteis</h3>
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Novidades</a></li>
+                    <li><a href="#">Promoções</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </div>
+            <div class="footer-column">
+                <h3>Contato</h3>
+                <ul>
+                    <li>Email: maricota01@gmail.com</li>
+                    <li>Telefone: (77) 5534-5378</li>
+                    <li>Endereço: Rua 10 de Maio, 453, Guanambi, BA</li>
+                </ul>
+            </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
