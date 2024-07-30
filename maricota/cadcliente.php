4a8f00083023a6a842a1d4141f75aaf0c3fc1416
josepedro10 <145406929+josepedro10@users.noreply.github.com>
2024-07-30 10:25:01 -0300

    Update cadcliente.php

--- maricota/cadcliente.php
+++ maricota/cadcliente.php
@@ -6,9 +6,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $senha = password_hash($_POST['senha'], PASSWORD_BCRYPT);
-    $cpf = $_POST['cpf']
+    $cpf = $_POST['cpf'];
 
-    $sql = "INSERT INTO usuarios (nome, email, senha, cpf) VALUES ('$nome', '$email', '$senha', '$cpf)";
+    $sql = "INSERT INTO usuarios (nome, email, senha, cpf) VALUES ('$nome', '$email', '$senha', '$cpf')";
     
     if ($conn->query($sql) === TRUE) {
         echo "Cadastro realizado com sucesso!";
