500f9d65b775a7e47356f1fb2ffbdd128eee0e12
josepedro10 <145406929+josepedro10@users.noreply.github.com>
2024-08-02 20:25:30 -0300

    0

--- maricota/index.php
+++ maricota/index.php
@@ -37,23 +37,27 @@
         <hr>
 
         <div class="linha1">
-            <div class="pr1">
-                <img src="./imagens/bota.jpg" alt="">
-                <p>Bota Frances Couro Prata</p>
-                <p>R$675,00</p>
-                <div class="adicionais">
-                <a href="produto.php" class="button-link"> Ver detalhes</a>
-                </div>
-            </div>
-            <div class="pr1">
-                <img src="./imagens/papete-dourada.jpg" alt="">
-                <p>Papete Couro Dourada</p>
-                <p>R$345,00</p>
+
+        <div class="pr1">
+                <img src="./imagens/sapato-mule.jpg" alt="">
+                <p>Mule New Carry</p>
+                <p>R$490,00</p>
                 <div class="adicionais">
                 <a href="produto.php" class="button-link"> Comprar</a>
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
             </div>
+            
+        <div class="pr1">
+                <img src="./imagens/sapato-scarpin.jpg" alt="">
+                <p>Scarpin Chain Ornamental</p>
+                <p>R$720,00</p>
+                <div class="adicionais">
+                <a href="produto.php" class="button-link"> Comprar</a>
+                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
+                </div>
+            </div>
+
             <div class="pr1">
                 <img src="./imagens/scarpin-vermelho.jpg" alt="">
                 <p>Scarpin Vermelho Couro</p>
@@ -63,6 +67,7 @@
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
             </div>
+
             <div class="pr1">
                 <img src="./imagens/scarpin-azul.jpg" alt="">
                 <p>Scarpin Azul Couro</p>
@@ -72,40 +77,126 @@
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
             </div>
+
+        </div>
+
+        <div class="linha1">
+
+            <div class="pr1">
+                <img src="./imagens/mule-curve.jpg" alt="">
+                <p>Mule Curve Heels Verniz</p>
+                <p>R$790,00</p>
+                <div class="adicionais">
+                <a href="produto.php" class="button-link"> Comprar</a>
+                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
+                </div>
+            </div>
+
+            <div class="pr1">
+                <img src="./imagens/mule-veronica.jpg" alt="">
+                <p>Sapato Mule Veronica Pump</p>
+                <p>R$790,00</p>
+                <div class="adicionais">
+                <a href="produto.php" class="button-link"> Comprar</a>
+                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
+                </div>
+            </div>
+
+            <div class="pr1">
+                <img src="./imagens/sapato-scarpin.jpg" alt="">
+                <p>Scarpin Slingback Salto Alto</p>
+                <p>R$650,00</p>
+                <div class="adicionais">
+                <a href="produto.php" class="button-link"> Comprar</a>
+                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
+                </div>
+            </div>
+        
         </div>
 
         <h1>Tenis</h1>
         <hr>
 
         <div class="linha2">
+
         <div class="pr2">
-                <img src="./imagens/sandalia-branca.jpg" alt="">
-                <p>Sandália Branca Tressê</p>
-                <p>R$439,90</p>
+                <img src="./imagens/tenis-schutz.jpg" alt="">
+                <p>Tênis Schutz ST 001</p>
+                <p>R$590,00</p>
+                <div class="adicionais">
+                <a href="produto.php" class="button-link"> Comprar</a>
+                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
+                </div>
+        </div>
+
+        <div class="pr2">
+                <img src="./imagens/tenis-vert.jpg" alt="">
+                <p>Tênis Vert Shoes Esplar</p>
+                <p>R$399,90</p>
+                <div class="adicionais">
+                <a href="produto.php" class="button-link"> Comprar</a>
+                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
+                </div>
+        </div>
+
+        <div class="pr2">
+                <img src="./imagens/tenis-adidas.jpg" alt="">
+                <p>Tênis Couro adidas</p>
+                <p>$519,00</p>
+                <div class="adicionais">
+                <a href="produto.php" class="button-link"> Comprar</a>
+                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
+                </div>
+        </div>
+
+        <div class="pr2">
+                <img src="./imagens/tenis-golden.jpg" alt="">
+                <p>Tênis cano baixo Supersta</p>
+                <p>R$2.309,00</p>
+                <div class="adicionais">
+                <a href="produto.php" class="button-link"> Comprar</a>
+                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
+                </div>
+        </div>
+
+
+        </div>
+
+        <h1>Sandalias</h1>
+        <hr>
+
+        <div class="linha3">
+
+        <div class="pr3">
+                <img src="./imagens/papete-chain.jpg" alt="">
+                <p>Papete Chain Ornamental</p>
+                <p>R$590,00</p>
                 <div class="adicionais">
                 <a href="produto.php" class="button-link"> Comprar</a>
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
             </div>
-            <div class="pr2">
-                <img src="./imagens/sandalia-preta.jpg" alt="">
-                <p>Sandália Preta Nobuck</p>
-                <p>R$339,90</p>
+        
+        <div class="pr3">
+                <img src="./imagens/papete-brilho.jpg" alt="">
+                <p>Papete Brilho Prata</p>
+                <p>R$470,00</p>
                 <div class="adicionais">
                 <a href="produto.php" class="button-link"> Comprar</a>
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
             </div>
-            <div class="pr2">
-                <img src="./imagens/sandalia-prata.jpg" alt="">
-                <p>Sandália Prata Couro</p>
-                <p>R$299,90</p>
+
+        <div class="pr3">
+                <img src="./imagens/papete-dourada.jpg" alt="">
+                <p>Papete Couro Dourada</p>
+                <p>R$345,00</p>
                 <div class="adicionais">
                 <a href="produto.php" class="button-link"> Comprar</a>
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
             </div>
-            <div class="pr2">
+        <div class="pr3">
                 <img src="./imagens/sandalia-papete.jpg" alt="">
                 <p>Papete Tiras Strass Preta</p>
                 <p>R$245,00</p>
@@ -113,22 +204,8 @@
                 <a href="produto.php" class="button-link"> Comprar</a>
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
-            </div>
         </div>
 
-        <h1>Sandalias</h1>
-        <hr>
-
-        <div class="linha3">
-        <div class="pr3">
-                <img src="./imagens/sapato-scarpin.jpg" alt="">
-                <p>Scarpin Chain Ornamental</p>
-                <p>R$720,00</p>
-                <div class="adicionais">
-                <a href="produto.php" class="button-link"> Comprar</a>
-                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
-                </div>
-            </div>
             <div class="pr3">
                 <img src="./imagens/papete-couro.jpg" alt="">
                 <p>Sandália Papete Couro</p>
@@ -138,39 +215,24 @@
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
             </div>
-            <div class="pr3">
-                <img src="./imagens/sapato-mule.jpg" alt="">
-                <p>Sapato Mule New Carry</p>
-                <p>R$490,00</p>
-                <div class="adicionais">
-                <a href="produto.php" class="button-link"> Comprar</a>
-                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
-                </div>
-            </div>
-            <div class="pr3">
-                <img src="./imagens/sandalia-new.jpg" alt="">
-                <p>Sandália New Icon Salto Bloco</p>
-                <p>R$590,00</p>
-                <div class="adicionais">
-                <a href="produto.php" class="button-link"> Comprar</a>
-                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
-                </div>
-            </div>
+
         </div>
 
         <h1>Saltos</h1>
         <hr>
 
         <div class="linha4">
+
         <div class="pr4">
-                <img src="./imagens/tenis-schutz.jpg" alt="">
-                <p>Tênis Schutz ST 001</p>
+                <img src="./imagens/sandalia-preta.jpg" alt="">
+                <p>Salto Médio Skye Leaf Couro</p>
                 <p>R$590,00</p>
                 <div class="adicionais">
                 <a href="produto.php" class="button-link"> Comprar</a>
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
-            </div>
+        </div>
+
             <div class="pr4">
                 <img src="./imagens/sandalia-salto.jpg" alt="">
                 <p>Salto Alto Pamela Verinz</p>
@@ -180,67 +242,69 @@
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
             </div>
+
             <div class="pr4">
-                <img src="./imagens/papete-brilho.jpg" alt="">
-                <p>Papete Brilho Prata</p>
-                <p>R$470,00</p>
+                <img src="./imagens/sandalia-new.jpg" alt="">
+                <p>Sandália New Icon Salto Bloco</p>
+                <p>R$590,00</p>
                 <div class="adicionais">
                 <a href="produto.php" class="button-link"> Comprar</a>
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
             </div>
-            <div class="pr4">
-                <img src="./imagens/sapato-scarpin.jpg" alt="">
-                <p>Scarpin Slingback Salto Alto</p>
-                <p>R$650,00</p>
+
+        </div>
+
+        <div class="linha4">
+
+        <div class="pr4">
+                <img src="./imagens/sandalia-branca.jpg" alt="">
+                <p>Sandália Branca Tressê</p>
+                <p>R$439,90</p>
                 <div class="adicionais">
                 <a href="produto.php" class="button-link"> Comprar</a>
                 <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
                 </div>
             </div>
+
+            <div class="pr4">
+                <img src="./imagens/sandalia-preta.jpg" alt="">
+                <p>Sandália Preta Nobuck</p>
+                <p>R$339,90</p>
+                <div class="adicionais">
+                <a href="produto.php" class="button-link"> Comprar</a>
+                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
+                </div>
+            </div>
+
+            <div class="pr4">
+                <img src="./imagens/sandalia-prata.jpg" alt="">
+                <p>Sandália Prata Couro</p>
+                <p>R$299,90</p>
+                <div class="adicionais">
+                <a href="produto.php" class="button-link"> Comprar</a>
+                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
+                </div>
+            </div>
+
         </div>
 
         <h1>Botas</h1>
         <hr>
 
         <div class="linha5">
+
         <div class="pr5">
-                <img src="./imagens/sandalia-preta.jpg" alt="">
-                <p>Salto Médio Skye Leaf Couro</p>
-                <p>R$590,00</p>
+                <img src="./imagens/bota.jpg" alt="">
+                <p>Bota Frances Couro Prata</p>
+                <p>R$675,00</p>
                 <div class="adicionais">
-                <a href="produto.php" class="button-link"> Comprar</a>
-                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
+                <a href="produto.php" class="button-link"> Ver detalhes</a>
                 </div>
             </div>
-            <div class="pr5">
-                <img src="./imagens/papete-chain.jpg" alt="">
-                <p>Papete Chain Ornamental</p>
-                <p>R$590,00</p>
-                <div class="adicionais">
-                <a href="produto.php" class="button-link"> Comprar</a>
-                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
-                </div>
-            </div>
-            <div class="pr5">
-                <img src="./imagens/mule-curve.jpg" alt="">
-                <p>Mule Curve Heels Verniz</p>
-                <p>R$790,00</p>
-                <div class="adicionais">
-                <a href="produto.php" class="button-link"> Comprar</a>
-                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
-                </div>
-            </div>
-            <div class="pr5">
-                <img src="./imagens/mule-veronica.jpg" alt="">
-                <p>Sapato Mule Veronica Pump</p>
-                <p>R$790,00</p>
-                <div class="adicionais">
-                <a href="produto.php" class="button-link"> Comprar</a>
-                <a href="carrinho.php" class="button-link">Adicionar ao carrinho</a>
-                </div>
-            </div>
+
         </div>
+    
     </main>
 
     <footer>
